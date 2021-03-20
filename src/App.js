640666a0f713a3f9b7dd9cb086ad0088a4b90a32
alona566 <alonaazure@gmail.com>
2021-03-19 19:55:17 -0500

    Profile still has a few errors that I need to fix but I wanted to push what I have so far

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { Switch, Route } from "react-router-dom";
 
 import Home from "./views/Home";
 import NotFound from "./views/NotFound";
+import Profile from './views/Profile';
+import Messages from './views/MessageList';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/messages" component={MessageList} />
+        <Route exact path="/profile" component={Profile} />
         <Route component={NotFound} />
+        
       </Switch>
     </div>
+  
   );
 }
 
--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,1 +1,84 @@
-// TODO: Create a Profile to display the current users information
+import React from 'react'
+import useForm from './useForm'
+import validate from './validateInfo'
+const ProfilePage = () => {
+  const { handleChange, values, handleSubmit, errors } = useForm(validate);
+
+  return (
+      <div className="form-content-right">
+          <form className="form" onSubmit={handleSubmit}>
+              <h1>Create your profile here.
+               </h1>
+               <div className="form-input">
+                   <lable htmlfor="profile" className="form-lable">
+                       Full name
+                   </lable>
+                   <input 
+                       id='profile'
+                      type='text' 
+                       name='fullName'
+                       className='form-input'
+                       placeholder=''
+                       value={values.fullName}
+                       onChange={handleChange}
+                       />
+                       {errors.fullName && <p>{errors.fullName}</p>}
+               </div>
+               <div className='form-input'>
+                   <lable htmlfor='Email' className='form-lable'>
+                       Email
+                   </lable>
+                   <input 
+                      id='email'
+                      type='email' 
+                       name='email'
+                       className='form-input'
+                       placeholder=''
+                       value={values.email}
+                       onChange={handleChange}
+                       />
+                       {errors.email && <p>{errors.email}</p>}
+               </div>
+               <div className='form-input'>
+                   <lable htmlfor='userName' className='form-lable'>
+                       User Name
+                   </lable>
+                   <input 
+                      id='userName'
+                      type='userName' 
+                       name='userName'
+                       className='form-input'
+                       placeholder=''
+                       value={values.userName}
+                       onChange={handleChange}
+                       />
+               </div>
+               <div className='form-input'>
+                   <lable htmlfor='bio' className='form-lable'>
+                       Tell Us About Yourself
+                   </lable>
+                   <input 
+                      id='bio'
+                      type='bio' 
+                       name='bio'
+                       className='form-input2'
+                       placeholder=''
+                       value={values.bio}
+                       onChange={handleChange}
+                       />
+                       {errors.bio && <p>{errors.bio}</p>}
+               </div>
+               <button className='form-input-btn' type='submit'>
+                  Submit Profile
+               </button>
+               <span className='form-input-login'>
+                    <a href="#"></a>
+               </span>
+          </form>
+      </div>
+  );
+};
+
+export default ProfilePage
+
+
