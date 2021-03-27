39169acc6d8c06f028b77fb6d47128ffc7b48cea
TERRY CROSS <tcrossband.86@gmail.com>
2021-03-26 21:12:20 -0700

    minor changes for gh-pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/kwitter-terry-cross">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
